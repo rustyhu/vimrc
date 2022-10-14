7280e6be1b953f4e4e186d1de494965989b9fb6a
rustyhu <rustyhu@hotmail.com>
2022-10-14 15:56:21 +0800

    Update ycmcompile.sh options.

--- ycmcompile.sh
+++ ycmcompile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 pushd ~/vimrc/bundle/YouCompleteMe
-python3 install.py --clang-completer --clangd-completer --go-completer --rust-completer --ts-completer
+python3 install.py --clang-completer --clangd-completer --rust-completer
+# --go-completer  #use vim-go instead
+# --ts-completer  #no scenario now
 popd
